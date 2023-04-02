2eea737037a6327232f8876142b5d97f72c0753a
unknown <jasonajordan0@gmail.com>
2023-04-02 16:24:22 -0400

    [TS]tailwind + basic setup

---
+++ src/components/Intro.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Intro = ({heading, message}) => {
+  return (
+    <div>
+      <div className='flex istems-center justify-center'>
+        Intro Comp
+      </div>
+      <div className= 'flex istems-center justify-center
+                      mb-12 bg-fixed bg-center bg-cover bg-white'>
+        <div className=''/>
+        <div className='p-5 ml-[-10rem] mt-[10rem]'>
+          <h2 className='text-5xl font-bold'>{heading}</h2>
+          <p className='py-5 text-xl'>{message}</p>
+          <button className='px-8 py-2 border'>Book</button>  
+        </div> 
+      </div>
+    </div>
+
+  )
+}
+
+export default Intro
